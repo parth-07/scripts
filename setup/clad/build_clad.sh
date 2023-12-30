29c644b50a4796f3094832f9bbfd868f3b048f42
parth <partharora99160808@gmail.com>
2023-12-30 18:32:11 +0530

    Update Clad scripts

--- setup/clad/build_clad.sh
+++ setup/clad/build_clad.sh
@@ -1,14 +1,22 @@
+#!/usr/bin/env bash
 SCRIPT_DIR=$(dirname ${0})
-if ! dpkg -s clang-${1} >/dev/null 2>&1
-then
-  echo "Installing dependencies."
-  ${SCRIPT_DIR}/install_clad_dependencies.sh ${1}
-fi
+# if ! dpkg -s clang-${1} >/dev/null 2>&1
+# then
+#   echo "Installing dependencies."
+#   ${SCRIPT_DIR}/install_clad_dependencies.sh ${1}
+# fi
 
-if [ "${2}" = "pristine" ]; then
-  BUILDS_DIR=/home/parth/clubs/open-source/clad/pristine/builds
+if [ -n "${2}" ]; then
+  BUILDS_DIR="/home/parth/clubs/open-source/clad/${2}/builds"
 else
   BUILDS_DIR=/home/parth/clubs/open-source/clad/fork/builds
+fi
+
+LLVM_DIR=/usr/lib/llvm-${1}
+CLANG_DIR=/usr/lib/llvm-${1}
+if [[ "$1" =~ "debug" ]]; then
+  LLVM_DIR="/home/parth/clubs/open-source/LLVM/builds/build-${1}/inst/lib/cmake/llvm"
+  CLANG_DIR="/home/parth/clubs/open-source/LLVM/builds/build-${1}/inst/lib/cmake/clang"
 fi
 
 mkdir -p ${BUILDS_DIR}/build-${1}
@@ -16,5 +24,5 @@ cd ${BUILDS_DIR}/build-${1}
 mkdir -p obj inst
 cd obj
 rm -rf *
-CC="clang-14" CXX="clang++-14" cmake -G Ninja ../../../clad -DClang_DIR=/usr/lib/llvm-${1} -DLLVM_DIR=/usr/lib/llvm-${1} -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../inst -DLLVM_EXTERNAL_LIT="`which lit`"
+CC="clang-15" CXX="clang++-15" cmake -G Ninja ../../../clad -DClang_DIR=${CLANG_DIR} -DLLVM_DIR=${LLVM_DIR} -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../inst -DLLVM_EXTERNAL_LIT="`which lit`" -DCMAKE_EXPORT_COMPILE_COMMANDS=On
 ninja install
--- setup/clad/update_clad.sh
+++ setup/clad/update_clad.sh
@@ -1,16 +1,16 @@
-if [ "${2}" = "pristine" ]; then
-  BUILDS_DIR=/home/parth/clubs/open-source/clad/pristine/builds
+if [ -n "${2}" ]; then
+  BUILDS_DIR="/home/parth/clubs/open-source/clad/${2}/builds"
 else
-BUILDS_DIR=/home/parth/clubs/open-source/clad/fork/builds
+  BUILDS_DIR=/home/parth/clubs/open-source/clad/fork/builds
 fi
 
 build_root=${BUILDS_DIR}/build-${1}
 
 if [ ! -d ${build_root} ]
-then 
+then
   echo "Build root do not exist!"
   echo "Configuring and building Clad."
-  build_clad.sh ${1}
+  build_clad.sh "${1}" "${2}"
 else
   echo "Updating Clad."
   cd ${build_root}/obj
